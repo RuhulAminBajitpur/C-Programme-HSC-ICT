c5cbe1293b61e4526adc248807ecf5dd62fbdcf1
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-14 16:39:11 +0600

    Create The largest number among the three numbers.c

---
+++ The largest number among the three numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 3 Number : ");
+    scanf("%d %d %d",&a,&b,&c);
+    if((a>b)&&(a>c))
+    {
+    printf(" The largest number is :%d",a);
+    }
+    else if((b>c)&&(b>a))
+    {
+    printf(" The largest number is :%d",b);
+    }
+    else
+    {
+    printf(" The largest number is :%d",c);
+    }
+    
+   
+    return 0;
+}
