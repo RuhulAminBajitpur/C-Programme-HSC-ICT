0743c2f4b4a25627d3c6f52fdfe1c2a7d235435a
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 18:30:03 +0600

    Create For Loop.c

---
+++ For Loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n;
+    printf("How many times you want to see the text?: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("Ruhul Amin\n");
+        
+    }
+    return 0;
+}
