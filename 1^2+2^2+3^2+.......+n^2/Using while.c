ecb0c5685235addcd3340dec14a6104d9b93de0a
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 20:24:42 +0600

    Create Using while.c

---
+++ 1^2+2^2+3^2+.......+n^2/Using while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int i=1,n,sum=0;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        sum=sum+i*i;
+        i++;
+    }
+    printf("Summation=%d",sum);
+    return 0;
+}
