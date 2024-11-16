a26af28ef1d43134a5b6bdd0df79ae2ae7ea075f
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 13:41:26 +0600

    Create ত্রিভুজের ক্ষেত্রফল.c

---
+++ ত্রিভুজের ক্ষেত্রফল.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float b,h,area;
+    printf("Enter base & height: ");
+    scanf("%f %f",&b,&h);
+    area=(b*h)/2;
+    printf("Area=%f",area);
+    return 0;
+}
