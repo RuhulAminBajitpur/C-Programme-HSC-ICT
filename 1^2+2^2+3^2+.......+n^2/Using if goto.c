7e9309c0cc4d5d9bdf6d548e90a70517ffcfd1ca
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 20:28:40 +0600

    Create Using if goto.c

---
+++ 1^2+2^2+3^2+.......+n^2/Using if goto.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int i=1,n,sum=0;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    level:
+    
+        sum=sum+i*i;
+        i++;
+    
+    if(i<=n)
+    goto level;
+    printf("Summation=%d",sum);
+    return 0;
+}
