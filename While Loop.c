263ccb2942a6d0630d09db4ab06689a6193d36fb
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 18:35:45 +0600

    Create While Loop.c

---
+++ While Loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n;
+    i=1;
+    printf("How many times you want to see the text?: ");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("Ruhul Amin\n");
+        i++;
+    }
+    return 0;
+}
