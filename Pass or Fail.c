be9427f5cae8e556470df779145d30f837412745
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 16:35:56 +0600

    Update Pass or Fail.c

--- Pass or Fail.c
+++ Pass or Fail.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your mark: ");
+    scanf("%d",&a);
+    if(a>=33)
+    printf("Pass");
+    else
+    printf("Fail");
+    return 0;
+}
