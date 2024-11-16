11804cef75d0123932e6105717b30f34e992d3a0
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 13:50:01 +0600

    Create বর্গক্ষেত্রের ক্ষেত্রফল.c

---
+++ বর্গক্ষেত্রের ক্ষেত্রফল.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float d,area;
+    printf("Enter the value of diagonal: ");
+    scanf("%f",&d);
+    area=(d*d)/2;
+    printf("Area=%f",area);
+    return 0;
+}
