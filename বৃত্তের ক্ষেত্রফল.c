5b0430e9885542bdea725216bbaf8d81b3de499f
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 13:54:56 +0600

    Create বৃত্তের ক্ষেত্রফল.c

---
+++ বৃত্তের ক্ষেত্রফল.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,area;
+    printf("Enter the value of radius: ");
+    scanf("%f",&r);
+    area=3.1416*r*r;
+    printf("Area=%f",area);
+    return 0;
+}
