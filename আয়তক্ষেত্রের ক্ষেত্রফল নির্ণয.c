9a3483badca519d21bd3fbed2fa27bef3d333947
Ruhul Amin <ruhulaminbajitpur.xyz@gmail.com>
2024-11-16 13:06:54 +0600

    Create আয়তক্ষেত্রের ক্ষেত্রফল নির্ণয.c

---
+++ আয়তক্ষেত্রের ক্ষেত্রফল নির্ণয.c
@@ -0,0 +1,11 @@
+//আয়তক্ষেত্রের ক্ষেত্রফল নির্ণয়।
+#include<stdio.h>
+int main()
+{
+    int l,w,a;
+    printf("Enter Length & Weight: ");
+    scanf("%d %d",&l,&w);
+    a=l*w;
+    printf("Area=%d",a);
+    return 0;
+}
